4c75505a48672fbf1e7901bacd50649f74aef881
AsmaDage <asmadage@gmail.com>
2022-01-17 15:45:44 +0000

    plans done

---
+++ workshop-data.js
@@ -0,0 +1,44 @@
+const workshopData = [
+  {
+    id: 1,
+    name: "Koala",
+    date: "17/01/2022",
+    progress: "I hate CSS",
+    mood: "poor",
+  },
+  {
+    id: 2,
+    name: "To-do list",
+    date: "16/01/2022",
+    progress: "Dynamic rendering was fun!",
+    mood: "good",
+  },
+  {
+    id: 3,
+    name: "React router",
+    date: "15/01/2022",
+    progress: "Struggled to create routes",
+    mood: "fair",
+  },
+  {
+    id: 4,
+    name: "Rock, paper, scissors",
+    date: "14/01/2022",
+    progress: "JS was a challenge!",
+    mood: "fair",
+  },
+  {
+    id: 5,
+    name: "Express",
+    date: "13/01/2022",
+    progress: "Express was great",
+    mood: "good",
+  },
+  {
+    id: 6,
+    name: "Node PG",
+    date: "12/01/2022",
+    progress: "Node was tough",
+    mood: "poor",
+  },
+];
