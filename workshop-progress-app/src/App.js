563290b0c68d3281d9c077773bbeaeadb3fff02d
sim89 <simrenkaursoor@gmail.com>
2022-01-17 16:54:05 +0000

    added html elements for radio buttons, input tags, label tags, add task button and adjusted css for user-form

---
+++ workshop-progress-app/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+   return (
+      <div className="App">
+         <form className="user-form">
+            <input type="text" name="Workshop-name" />
+            <input type="text" name="Date" />
+            <input type="text" name="Progress" />
+
+            <input type="radio" id="poor" name="mood" value="poor" />
+            <label for="poor">poor</label>
+            <br />
+            <input type="radio" id="fair" name="mood" value="fair" />
+            <label for="fair">fair</label>
+            <br />
+            <input type="radio" id="good" name="mood" value="good" />
+            <label for="good">good</label>
+            <br />
+            <br />
+            <button type="submit">Add Task</button>
+         </form>
+      </div>
+   );
+}
+
+export default App;
