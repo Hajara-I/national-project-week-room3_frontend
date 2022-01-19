cf7de0976c5d27c59a04710ea20765e65a8e10e2
sim89 <simrenkaursoor@gmail.com>
2022-01-19 09:36:47 +0000

    added emojis

--- workshop-progress-app/src/components/App/index.js
+++ workshop-progress-app/src/components/App/index.js
@@ -1,72 +1,73 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import UserInput from "../UserInput";
 import { WorkshopList } from "../WorkshopList";
 import { sampleData } from "./workshop-data";
 
 function App() {
-  const [text, setText] = useState("");
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "Koala",
-      date: "17/01/2022",
-      progress: 50,
-      mood: "poor",
-    },
-  ]);
+   const [text, setText] = useState("");
+   const [data, setData] = useState([
+      {
+         id: 1,
+         name: "Koala",
+         date: "17/01/2022",
+         progress: 50,
+         mood: "poor",
+      },
+   ]);
 
-  const handleChange = (event) => {
-    const input = event.target.value;
-    setText(input);
-    // console.log("handleChange function has been called", value);
-  };
+   const handleChange = (event) => {
+      const input = event.target.value;
+      setText(input);
+      // console.log("handleChange function has been called", value);
+   };
 
-  const handleClick = (e) => {
-    setText("");
-    e.preventDefault();
-    const newObject = {
-      id: 2,
-      name: text,
-      date: "18/01/2022",
-      progress: 100,
-      mood: "good",
-    };
-    setData([...data, newObject]);
-    console.log(data);
-  };
+   const handleClick = (e) => {
+      setText("");
+      e.preventDefault();
+      const newObject = {
+         id: 2,
+         name: text,
+         date: "18/01/2022",
+         progress: 100,
+         mood: "good",
+      };
+      setData([...data, newObject]);
+      console.log(data);
+   };
 
-  // function handleClick(e) {
-  //    const value = e.target.progress.value;
-  //    setData(value);
-  //    console.log("handleClick has been called",value);
-  // }
+   // function handleClick(e) {
+   //    const value = e.target.progress.value;
+   //    setData(value);
+   //    console.log("handleClick has been called",value);
+   // }
 
-  // function addNewWorkshop () {
-  //    const newData = {name: document.querySelector(".berries").value};
-  //    setData({...data, newData})
+   // function addNewWorkshop () {
+   //    const newData = {name: document.querySelector(".berries").value};
+   //    setData({...data, newData})
 
-  // }
+   // }
 
-  return (
-    <section>
-      <UserInput handleClick={handleClick} handleChange={handleChange} />
-      <div>
-        {data.map(function (item) {
-          return (
-            <div>
-              <p key={item.id}></p>
-              <p>{item.name}</p>
-              <p>{item.date}</p>
-              <p>{item.progress}</p>
-              <p>{item.mood}</p>
-            </div>
-          );
-        })}
-      </div>
-      {/* <WorkshopList /> */}
-    </section>
-  );
+   return (
+      <section>
+         <UserInput handleClick={handleClick} handleChange={handleChange} />
+         <div>
+            {data.map(function (item) {
+               return (
+                  <div>
+                     <p key={item.id}></p>
+                     <p>{item.name}</p>
+                     <p>{item.date}</p>
+                     <p>{item.progress}</p>
+                     <p>{item.mood}</p>
+                  </div>
+               );
+            })}
+         </div>
+
+         {/* <WorkshopList /> */}
+      </section>
+   );
 }
 
 export default App;
--- workshop-progress-app/src/components/UserInput/index.js
+++ workshop-progress-app/src/components/UserInput/index.js
@@ -1,37 +1,54 @@
 import React from "react";
 
 function UserInput({ handleChange, handleClick }) {
-  return (
-    <div className="App">
-      <form className="user-form">
-        <p>Workshop Name</p>
-        <input
-          type="text"
-          className="berries"
-          name="workshop"
-          placeholder="Enter a workshop name"
-          onChange={handleChange}
-        />
-        <input type="date" name="Date" />
-        <input name="Progress" placeholder="How far did you get?" />
-        <br />
-        <br />
-        <hr />
-        <input type="radio" className="emoji" name="mood" value="poor" />
-        <label>:(</label>
+   return (
+      <div className="App">
+         <form className="user-form">
+            <p>Workshop Name</p>
+            <input
+               type="text"
+               className="berries"
+               name="workshop"
+               placeholder="Enter a workshop name"
+               onChange={handleChange}
+            />
+            <input type="date" name="Date" />
+            <input name="Progress" placeholder="How far did you get?" />
+            <br />
+            <br />
+            <hr />
 
-        <input type="radio" className="emoji" name="mood" value="fair" />
-        <label>:|</label>
+            <input type="radio" className="emoji" name="mood" value="good" />
+            <span aria-label=" grin smile" role="img">
+               😃
+            </span>
 
-        <input type="radio" className="emoji" name="mood" value="good" />
-        <label>:)</label>
+            <input type="radio" className="emoji" name="mood" value="good" />
+            <span aria-label="simple smile" role="img">
+               🙂
+            </span>
 
-        <br />
-        <br />
-        <button onClick={handleClick}>Add Task</button>
-      </form>
-    </div>
-  );
+            <input type="radio" className="emoji" name="mood" value="fair" />
+            <span aria-label="neutral face" role="img">
+               😐
+            </span>
+
+            <input type="radio" className="emoji" name="mood" value="poor" />
+            <span aria-label="disappointed face" role="img">
+               😞
+            </span>
+
+            <input type="radio" className="emoji" name="mood" value="poor" />
+            <span aria-label="weary" role="img">
+               😩
+            </span>
+
+            <br />
+            <br />
+            <button onClick={handleClick}>Add Task</button>
+         </form>
+      </div>
+   );
 }
 
 export default UserInput;
