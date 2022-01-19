2e2fe0f52f9b98983b982760bac17247a6c5faab
AsmaDage <asmadage@gmail.com>
2022-01-19 16:46:21 +0000

    fetching data

--- workshop-progress-app/src/components/App/index.js
+++ workshop-progress-app/src/components/App/index.js
@@ -1,48 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 //import { sampleData } from "./workshop-data";
 import InputForm from "../InputForm";
 import Workshops from "../Workshops";
 
 function App() {
-	const [workshops, setWorkshops] = useState([]);
-	//const [workshopEntry, setWorkshopEntry] = useState();
+  const [workshops, setWorkshops] = useState([]);
+  //const [workshopEntry, setWorkshopEntry] = useState();
 
-	function addNewWorkshop() {
-		const newWorkshop = {
-			name: document.querySelector(".workshop-name").value,
-			date: document.querySelector(".date").value,
-			progress: document.querySelector(".progress").value,
-			mood: document.querySelector(".mood-btns").value,
-		};
-		setWorkshops([...workshops, newWorkshop]);
-		console.log(workshops);
-	}
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`https://workshop-tracker.herokuapp.com/`);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log("Not working");
+      }
+    };
+    getData();
+  }, []);
 
-	//function deleteWorkshop() {}
+  function addNewWorkshop() {
+    const newWorkshop = {
+      name: document.querySelector(".workshop-name").value,
+      date: document.querySelector(".date").value,
+      progress: document.querySelector(".progress").value,
+      mood: document.querySelector(".mood-btns").value,
+    };
+    setWorkshops([...workshops, newWorkshop]);
+    console.log(workshops);
+  }
 
-	return (
-		<div className="App">
-			<div className="form-container">
-				<InputForm onClick={addNewWorkshop} />
-			</div>
-			<br />
-			<br />
-			<div className="workshop-container">
-				{workshops.map(function (item, index) {
-					return (
-						<Workshops
-							key={index}
-							name={item.name}
-							date={item.date}
-							progress={item.progress}
-							mood={item.mood}
-						/>
-					);
-				})}
-			</div>
-		</div>
-	);
+  //function deleteWorkshop() {}
+
+  return (
+    <div className="App">
+      <div className="form-container">
+        <InputForm onClick={addNewWorkshop} />
+      </div>
+      <br />
+      <br />
+      <div className="workshop-container">
+        {workshops.map(function (item, index) {
+          return (
+            <Workshops
+              key={index}
+              name={item.name}
+              date={item.date}
+              progress={item.progress}
+              mood={item.mood}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
