5312781a2bf3e285712de78df19642f64154e917
Hajara Iyal <hajaraiyal@gmail.com>
2022-01-19 16:25:38 +0000

    commented out workshopList, sampleData

--- workshop-progress-app/src/components/App/index.js
+++ workshop-progress-app/src/components/App/index.js
@@ -1,83 +1,83 @@
 import React, { useState } from "react";
 import "./App.css";
 import UserInput from "../UserInput";
-import { WorkshopList } from "../WorkshopList";
-import { sampleData } from "./workshop-data";
+// import { WorkshopList } from "../WorkshopList";
+// import { sampleData } from "./workshop-data";
 
 function App() {
-   const [text, setText] = useState("");
-   const [data, setData] = useState([
-      {
-         id: 10,
-         name: "Express",
-         date: "17/01/2022",
-         progress: 50,
-         mood: "fair",
-      },
-   ]);
+	const [text, setText] = useState("");
+	const [data, setData] = useState([
+		{
+			id: 10,
+			name: "Express",
+			date: "17/01/2022",
+			progress: 50,
+			mood: "fair",
+		},
+	]);
 
-   // const handleChange = (e) => {
-   //    e.preventDefault();
-   //    const newObject = {
-   //       id: data.length + 1,
-   //       name: e.target.name,
-   //       date: e.target.date,
-   //       progress: e.target.progress,
-   //       mood: e.target.mood,
-   //    };
-   //    setData([{...data, newObject}]);
-   // };
+	// const handleChange = (e) => {
+	//    e.preventDefault();
+	//    const newObject = {
+	//       id: data.length + 1,
+	//       name: e.target.name,
+	//       date: e.target.date,
+	//       progress: e.target.progress,
+	//       mood: e.target.mood,
+	//    };
+	//    setData([{...data, newObject}]);
+	// };
 
-   // console.log("App re-rendered. ", data);
+	// console.log("App re-rendered. ", data);
 
-   // const handleSubmit = (e) => {
-   //    e.preventDefault();
-   // };
-   const handleChange = (event) => {
-      const input = event.target.value;
-      setText(input);
-      console.log(input, text);
-   };
+	// const handleSubmit = (e) => {
+	//    e.preventDefault();
+	// };
+	const handleChange = (event) => {
+		const input = event.target.value;
+		setText(input);
+		console.log(input, text);
+	};
 
-   const handleClick = (e) => {
-      setText("");
-      e.preventDefault();
+	const handleClick = (e) => {
+		setText("");
+		e.preventDefault();
 
-      const newObject = {
-         id: data.length + 1,
-         name: text,
-         date: text.date,
-         progress: text.progress,
-         mood: text.mood,
-      };
-      console.log(e.target.date);
-      setData([...data, newObject]);
-   };
+		const newObject = {
+			id: data.length + 1,
+			name: text,
+			date: text.date,
+			progress: text.progress,
+			mood: text.mood,
+		};
+		console.log(e.target.date);
+		setData([...data, newObject]);
+	};
 
-   return (
-      <section>
-         <UserInput
-            handleClick={handleClick}
-            handleChange={handleChange}
-            data={data}
-         />
-         <div className="data">
-            {data.map(function (item) {
-               return (
-                  <div key={item.id}>
-                     <p>{item.id}</p>
-                     <p>{item.name}</p>
-                     <p>{item.date}</p>
-                     <p>{item.progress}</p>
-                     <p>{item.mood}</p>
-                  </div>
-               );
-            })}
-         </div>
+	return (
+		<section>
+			<UserInput
+				handleClick={handleClick}
+				handleChange={handleChange}
+				data={data}
+			/>
+			<div className="data">
+				{data.map(function (item) {
+					return (
+						<div key={item.id}>
+							<p>{item.id}</p>
+							<p>{item.name}</p>
+							<p>{item.date}</p>
+							<p>{item.progress}</p>
+							<p>{item.mood}</p>
+						</div>
+					);
+				})}
+			</div>
 
-         {/* <WorkshopList /> */}
-      </section>
-   );
+			{/* <WorkshopList /> */}
+		</section>
+	);
 }
 
 export default App;
