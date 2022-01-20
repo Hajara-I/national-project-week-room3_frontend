4d08a7e052219204abc31354140376e1276e7fc7
AsmaDage <asmadage@gmail.com>
2022-01-20 18:00:21 +0000

    map fixed and add working

--- workshop-progress-app/src/components/InputForm/index.js
+++ workshop-progress-app/src/components/InputForm/index.js
@@ -1,63 +1,114 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function InputForm(props) {
-	return (
-		<div>
-			<div>
-				<input className="workshop-name" />
-			</div>
-			<div>
-				<input className="date" type="date" />
-			</div>
-			<div>
-				<label>Select your progress </label>
-			
-			<select className="progress" name="progress">
-				<option value="0">0%</option>
-				<option value="25">25%</option>
-				<option value="50">50%</option>
-				<option value="100">100%</option>
-			</select>
-			</div>
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [progress, setProgress] = useState("0");
+  const [mood, setMood] = useState("good");
+  const [isloading, setIsLoading] = useState(false);
 
-			<div>
-			<label>Select your mood </label>
-			<select className="mood-btns" name="mood">
-				<option
-					aria-label="weary"
-					role="img"
-					value="poor"
-					className="mood-poor"
-				>
-					{" "}
-					😩{" "}
-				</option>
-				<option
-					aria-label="neutral face"
-					role="img"
-					value="fair"
-					className="mood-fair"
-				>
-					{" "}
-					😐{" "}
-				</option>
-				<option
-					aria-label="grin face"
-					role="img"
-					value="good"
-					className="mood-good"
-				>
-					{" "}
-					😀{" "}
-				</option>
-			</select>
-			</div>
+  //   useNavigate is helpful for redirecting the user
+  const navigate = useNavigate();
 
-			<button className="submit-btn" onClick={props.onClick}>
-				SUBMIT
-			</button>
-		</div>
-	);
+  // creating a workshop object to POST into db
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const workshop = { name, date, progress, mood };
+
+    //setting state to inform user that post is pending
+    setIsLoading(true);
+
+    fetch("https://workshop-tracker.herokuapp.com/workshops", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(workshop),
+    }).then(() => {
+      console.log("New workshop added!");
+      setIsLoading(false);
+      // redirecting the user to the home path after they've added a workshop
+      navigate("/all-workshops");
+    });
+  };
+
+  return (
+    <div>
+      <h2>Add a Workshop</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            className="workshop-name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="date"
+            type="date"
+            required
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Select your progress </label>
+
+          <select
+            className="progress"
+            name="progress"
+            value={progress}
+            onChange={(e) => setProgress(e.target.value)}
+          >
+            <option value="0">0%</option>
+            <option value="25">25%</option>
+            <option value="50">50%</option>
+            <option value="100">100%</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Select your mood </label>
+          <select
+            className="mood-btns"
+            name="mood"
+            value={mood}
+            onChange={(e) => setMood(e.target.value)}
+          >
+            <option
+              aria-label="weary"
+              role="img"
+              value="poor"
+              className="mood-poor"
+            >
+              {" "}
+              😩{" "}
+            </option>
+            <option
+              aria-label="neutral face"
+              role="img"
+              value="fair"
+              className="mood-fair"
+            >
+              {" "}
+              😐{" "}
+            </option>
+            <option
+              aria-label="grin face"
+              role="img"
+              value="good"
+              className="mood-good"
+            >
+              {" "}
+              😀{" "}
+            </option>
+          </select>
+        </div>
+        <button className="submit-btn">SUBMIT</button>
+      </form>
+    </div>
+  );
 }
 
 export default InputForm;
--- workshop-progress-app/src/components/WorkshopList/index.js
+++ workshop-progress-app/src/components/WorkshopList/index.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
 const WorkshopList = ({ workshops, title }) => {
-	return (
-		<div className="workshop-list">
-			<h3>{title}</h3>
-			{workshops.map((workshop) => (
-				<div className="workshop-preview" key={workshop.payload.id}>
-					<h3>{workshop.payload.name}</h3>
-					<p>{workshop.payload.date}</p>
-					<p>You completed {workshop.payload.progress}%</p>
-					<p>You felt {workshop.payload.mood}</p>
-					<Link to={`/workshops/${workshop.payload.id}`}>
-						<button>Edit</button>
-					</Link>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="workshop-list">
+      <h3>{title}</h3>
+      {workshops.payload.map((workshop) => (
+        <div className="workshop-preview" key={workshop.id}>
+          <h3>{workshop.name}</h3>
+          <p>{workshop.date}</p>
+          <p>You completed {workshop.progress}%</p>
+          <p>You rated your performance as: {workshop.mood}</p>
+          <Link to={`/workshops/${workshop.id}`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default WorkshopList;
