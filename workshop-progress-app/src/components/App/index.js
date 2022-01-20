785638df9852bfc6737f6b986a9b1f335f3db22f
sim89 <simrenkaursoor@gmail.com>
2022-01-20 15:32:38 +0000

    deployed front end

--- workshop-progress-app/src/components/App/index.js
+++ workshop-progress-app/src/components/App/index.js
@@ -3,59 +3,74 @@ import "./App.css";
 //import { sampleData } from "./workshop-data";
 import InputForm from "../InputForm";
 import Workshops from "../Workshops";
+import { API_URL } from "../../config.js/index.js";
 
+//const API_URL = "http://localhost:3001/";
 function App() {
-  const [workshops, setWorkshops] = useState([]);
-  //const [workshopEntry, setWorkshopEntry] = useState();
+   const [workshops, setWorkshops] = useState([]);
+   //const [workshopEntry, setWorkshopEntry] = useState();
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(`https://workshop-tracker.herokuapp.com/`);
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log("Not working");
+   useEffect(() => {
+      async function getWorkshops() {
+         try {
+            const response = await fetch(
+               `${API_URL}workshops`
+               //method: "GET",
+               //headers: { "Content-Type": "application/json" },
+            );
+            const data = await response.json();
+            if (data.success === true) {
+               console.log(data);
+               //setUsers(data.payload);
+               //setError("");
+            } else {
+               // setError("Fetch didn't work :(");
+               console.log("Fetch didn't work");
+            }
+         } catch (err) {
+            //console.warn(jqxhr.responseText);
+            console.log(err);
+            // setError(err.message);
+         }
       }
-    };
-    getData();
-  }, []);
+      getWorkshops();
+   }, []);
 
-  function addNewWorkshop() {
-    const newWorkshop = {
-      name: document.querySelector(".workshop-name").value,
-      date: document.querySelector(".date").value,
-      progress: document.querySelector(".progress").value,
-      mood: document.querySelector(".mood-btns").value,
-    };
-    setWorkshops([...workshops, newWorkshop]);
-    console.log(workshops);
-  }
+   function addNewWorkshop() {
+      const newWorkshop = {
+         name: document.querySelector(".workshop-name").value,
+         date: document.querySelector(".date").value,
+         progress: document.querySelector(".progress").value,
+         mood: document.querySelector(".mood-btns").value,
+      };
+      setWorkshops([...workshops, newWorkshop]);
+      console.log(workshops);
+   }
 
-  //function deleteWorkshop() {}
+   //function deleteWorkshop() {}
 
-  return (
-    <div className="App">
-      <div className="form-container">
-        <InputForm onClick={addNewWorkshop} />
+   return (
+      <div className="App">
+         <div className="form-container">
+            <InputForm onClick={addNewWorkshop} />
+         </div>
+         <br />
+         <br />
+         <div className="workshop-container">
+            {workshops.map(function (item, index) {
+               return (
+                  <Workshops
+                     key={index}
+                     name={item.name}
+                     date={item.date}
+                     progress={item.progress}
+                     mood={item.mood}
+                  />
+               );
+            })}
+         </div>
       </div>
-      <br />
-      <br />
-      <div className="workshop-container">
-        {workshops.map(function (item, index) {
-          return (
-            <Workshops
-              key={index}
-              name={item.name}
-              date={item.date}
-              progress={item.progress}
-              mood={item.mood}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+   );
 }
 
 export default App;
