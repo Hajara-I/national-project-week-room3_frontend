8a024b7d1389a9d8977b32c0fe83d1a159ca8382
theusufructuary <pentamingforall@gmail.com>
2022-01-21 11:58:24 +0000

    Fixed edit button element bug on all workshops page

--- workshop-progress-app/src/components/WorkshopList/index.js
+++ workshop-progress-app/src/components/WorkshopList/index.js
@@ -36,11 +36,13 @@ const WorkshopList = ({ workshops, title }) => {
                 {workshop.mood}
               </p>
             </div>
-            <Link to={`/workshops/${workshop.id}`}>
+
               <div className="preview-btns-container">
+              <Link to={`/workshops/${workshop.id}`}>
                 <button className="edit-btn">Edit</button>
+                </Link>
               </div>
-            </Link>
+
           </div>
         );
       })}
